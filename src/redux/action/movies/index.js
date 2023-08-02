42e139ccf22cf2a2211a85d3707a1c9297efb9c3
KSHDestiny <kaungsathein.5899@gmail.com>
2023-08-02 06:35:45 +0630

    Day-1

---
+++ src/redux/action/movies/index.js
@@ -0,0 +1,15 @@
+import { ActionType } from "../action-types";
+
+export const fetchMovies = (movies) => {
+  return {
+    type: ActionType.FETCH_MOIVES,
+    payload: movies,
+  };
+};
+
+export const selectedMovie = (movie) => {
+  return {
+    type: ActionType.SELECT_MOVIE,
+    payload: movie,
+  };
+};
