0d7d64c67a58186d8b2b22feb5ec91d3901fbe60
KSHDestiny <kaungsathein.5899@gmail.com>
2023-08-03 06:08:09 +0630

    Data Searching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
-import { Navbar } from "flowbite-react";
+import { Navbar, TextInput } from "flowbite-react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import { api, api_key } from "../api";
+import { useDispatch } from "react-redux";
+import { fetchMovies } from "../redux/action/movies";
 
 const Header = () => {
+  // const [movieName, setMovieName] = useState("");
+  const movieName = useRef("");
+  const dispatch = useDispatch();
+
+  const searchMovie = async () => {
+    // console.log(movieName);
+    const search = movieName.current.value;
+    if (search !== "") {
+      const res = await api.get(
+        `/search/movie?query=${search}&api_key=${api_key}`
+      );
+
+      dispatch(fetchMovies(res.data.results));
+    } else {
+      const res = await api.get(`/movie/now_playing?api_key=${api_key}`);
+      dispatch(fetchMovies(res.data.results));
+    }
+  };
   return (
     <div>
       <Navbar fluid rounded>
@@ -10,7 +32,25 @@ const Header = () => {
             Code-Lab Movie Channel
           </span>
         </Link>
-        <div className="flex md:order-2"></div>
+        <div className="flex md:order-2">
+          <form action="">
+            <div className="flex justify-center align-middle">
+              <TextInput
+                placeholder="Search Movie..."
+                // value={movieName}
+                // onChange={(e) => setMovieName(e.target.value)}
+                ref={movieName}
+              />
+              <button
+                type="button"
+                className="bg-gray-900 text-white p-2 rounded-lg ms-2"
+                onClick={() => searchMovie()}
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
       </Navbar>
     </div>
   );
